f7ac47e93aec686361ad60a9dc7d4419384f7cbf
Handy Dorceus <dorceus.handy@gmail.com>
2024-07-12 17:05:25 -0400

    generating main site and sanity cms

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // Serve the main home.html for the root path
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'home.html'));
 });
 
 // Redirect any unknown paths under /admin to the index.html of the Sanity CMS
@@ -28,7 +28,7 @@ app.get('/admin/*', (req, res) => {
 
 // Handle other paths by serving the main site's home.html (for SPA support)
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'home.html'));
 });
 
 app.listen(port, () => {
