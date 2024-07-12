e5fc1d8cdc3e2a3a79f97a3630a534e25560f400
Handy Dorceus <dorceus.handy@gmail.com>
2024-07-12 16:32:11 -0400

    updated vercel.json

--- sanity-studio/sanity.config.js
+++ sanity-studio/sanity.config.js
@@ -3,13 +3,12 @@ import { structureTool } from 'sanity/structure';
 import { visionTool } from '@sanity/vision';
 import { schemaTypes } from './schemaTypes';
 
+
 export default defineConfig({
   name: 'default',
   title: 'valueform2',
-
   projectId: 'wfwxz1rq',
   dataset: 'production',
-
   basePath: '/admin', // Adding basePath for Sanity Studio
 
   plugins: [structureTool(), visionTool()],
